778290b3cd7970123237337e09fd0f2e55b0a97e
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2018-11-07 10:13:16 +0000

    fix the tests

--- test/flow.js
+++ test/flow.js
@@ -1,57 +1,36 @@
 const { expect } = require('chai');
 const { getAllSteps, getNextSteps } = require('../lib/flow');
+const {
+  returnedToApplicant,
+  withNtco,
+  ntcoEndorsed,
+  withLicensing,
+  referredToInspector,
+  inspectorRecommended,
+  inspectorRejected
+} = require('../lib/flow/status');
 
 describe('Flows', () => {
 
-  it('supports models that only ever autoresolve', () => {
-    expect(getAllSteps('certificate')).to.have.members(['autoresolved']);
-    expect(getAllSteps('exemption')).to.have.members(['autoresolved']);
-    expect(getAllSteps('invitation')).to.have.members(['autoresolved']);
-    expect(getAllSteps('profile')).to.have.members(['autoresolved']);
-  });
-
-  it('inherits the default flow', () => {
-    expect(getAllSteps('establishment')).to.include.members([
-      'autoresolved',
-      'returned-to-applicant',
-      'with-licensing',
-      'referred-to-inspector',
-      'resolved'
-    ]);
-  });
-
-  it('can provide all the available steps for a PIL flow', () => {
-    expect(getAllSteps('pil')).to.have.members([
-      'autoresolved',
-      'returned-to-applicant',
-      'submitted-to-ntco',
-      'with-licensing',
-      'referred-to-inspector',
-      'resolved'
+  it('can provide all the available steps', () => {
+    expect(getAllSteps()).to.have.members([
+      returnedToApplicant,
+      withNtco,
+      ntcoEndorsed,
+      withLicensing,
+      referredToInspector,
+      inspectorRecommended,
+      inspectorRejected
     ]);
   });
 
   it('can provide the next steps for a case', () => {
-    expect(getNextSteps('pil', 'submitted-to-ntco')).to.have.members([
-      'returned-to-applicant',
-      'with-licensing'
-    ]);
-  });
-
-  it('returns the default flow if there is no flow defined for a model', () => {
-    expect(getAllSteps('not-a-model')).to.have.members([
-      'autoresolved',
-      'returned-to-applicant',
-      'with-licensing',
-      'referred-to-inspector',
-      'resolved'
-    ]);
+    expect(getNextSteps(withNtco)).to.have.members([ntcoEndorsed, returnedToApplicant]);
   });
 
   it('returns an empty array of next steps if the step is not known', () => {
     /* eslint-disable no-unused-expressions */
-    expect(getNextSteps('pil', 'not-a-step')).to.be.an('array').that.is.empty;
-    expect(getNextSteps('not-a-model', 'not-a-step')).to.be.an('array').that.is.empty;
+    expect(getNextSteps('not-a-step')).to.be.an('array').that.is.empty;
     /* eslint-enable no-unused-expressions */
   });
 });
