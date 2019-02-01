b7f6b2f537e11f013f5f935fda4b7a7b69dc99e6
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2019-02-01 15:17:59 +0000

    add endpoint for fetching tasks associated with a model

---
+++ test/integration/tasks-for-model/index.js
@@ -0,0 +1,41 @@
+const request = require('supertest');
+const workflowHelper = require('../../helpers/workflow');
+const { user } = require('../../data/profiles');
+const ids = require('../../data/ids');
+const assertTasks = require('../../helpers/assert-tasks');
+
+describe('Tasks for a model', () => {
+  before(() => {
+    return workflowHelper.create()
+      .then(workflow => {
+        this.workflow = workflow;
+        this.workflow.setUser({ profile: user });
+      });
+  });
+
+  beforeEach(() => {
+    return Promise.resolve()
+      .then(() => workflowHelper.resetDBs())
+      .then(() => workflowHelper.seedTaskList());
+  });
+
+  after(() => {
+    return workflowHelper.destroy();
+  });
+
+  describe('outstanding tasks', () => {
+
+    it('sees tasks for a specific model id', () => {
+      const expected = ['pil with ntco'];
+
+      return request(this.workflow)
+        .get(`/model-tasks/${ids.pil.applied}`)
+        .expect(200)
+        .expect(response => {
+          assertTasks(expected, response.body.data);
+        });
+    });
+
+  });
+
+});
