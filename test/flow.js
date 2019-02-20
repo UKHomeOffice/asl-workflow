fa8aae65c70a6de0f1e2d308eccc8acc26530f17
Joe Fitter <joefitter@hotmail.com>
2019-02-20 15:00:03 +0000

    fix tests

--- test/flow.js
+++ test/flow.js
@@ -13,7 +13,7 @@ describe('Flows', () => {
 
   it('can provide the next steps for a case', () => {
     expect(getNextSteps(withNtco.id)).to.have.members([ntcoEndorsed, returnedToApplicant]);
-    expect(getNextSteps(referredToInspector.id)).to.have.members([inspectorRecommended, inspectorRejected]);
+    expect(getNextSteps(referredToInspector.id)).to.have.members([inspectorRecommended, inspectorRejected, returnedToApplicant]);
   });
 
   it('returns an empty array of next steps if the step is not known', () => {
