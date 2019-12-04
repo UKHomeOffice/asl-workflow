2c0c92e617a7a4bca49993037a3dcc98a03904e4
Joe Fitter <joefitter@hotmail.com>
2019-12-04 08:35:22 +0000

    Feature - Added place task create hook
    * Check changedBy when a place task is created
    * Send to inspector if not-asru
    * Send to licensing if inspector
    * Autoresolve if licensing

---
+++ test/unit/hooks/create/place.js
@@ -0,0 +1,70 @@
+const assert = require('assert');
+const sinon = require('sinon');
+const { withLicensing, withInspectorate, autoResolved } = require('../../../../lib/flow/status');
+const hook = require('../../../../lib/hooks/create/place');
+const Database = require('../../../helpers/asl-db');
+const fixtures = require('../../../data');
+
+const settings = {
+  host: process.env.ASL_DATABASE_HOST || 'localhost',
+  database: process.env.ASL_DATABASE_NAME || 'asl-test',
+  user: process.env.ASL_DATABASE_USERNAME || 'postgres'
+};
+
+const INSPECTOR_ID = 'a942ffc7-e7ca-4d76-a001-0b5048a057d2';
+const LICENSING_ID = 'a942ffc7-e7ca-4d76-a001-0b5048a057d1';
+const PELH_ID = 'ae28fb31-d867-4371-9b4f-79019e71232f';
+
+describe('Role create hook', () => {
+  before(() => {
+    return Database(settings).init(fixtures.default, true)
+      .then(models => {
+        this.models = models;
+        this.hook = hook({ models });
+      });
+  });
+
+  beforeEach(() => {
+    this.model = {
+      data: {
+        action: 'create',
+        data: {}
+      },
+      setStatus: sinon.stub()
+    };
+  });
+
+  after(() => {
+    this.models.destroy();
+  });
+
+  it('is sent to inspectorate if submitted by an establishment user', () => {
+    this.model.data.changedBy = PELH_ID;
+    return Promise.resolve()
+      .then(() => this.hook(this.model))
+      .then(() => {
+        assert.ok(this.model.setStatus.calledOnce);
+        assert.equal(this.model.setStatus.lastCall.args[0], withInspectorate.id);
+      });
+  });
+
+  it('autoresolves if submitted by a licensing officer', () => {
+    this.model.data.changedBy = LICENSING_ID;
+    return Promise.resolve()
+      .then(() => this.hook(this.model))
+      .then(() => {
+        assert.ok(this.model.setStatus.calledOnce);
+        assert.equal(this.model.setStatus.lastCall.args[0], autoResolved.id);
+      });
+  });
+
+  it('is sent to licensing if submitted by an inspector', () => {
+    this.model.data.changedBy = INSPECTOR_ID;
+    return Promise.resolve()
+      .then(() => this.hook(this.model))
+      .then(() => {
+        assert.ok(this.model.setStatus.calledOnce);
+        assert.equal(this.model.setStatus.lastCall.args[0], withLicensing.id);
+      });
+  });
+});
