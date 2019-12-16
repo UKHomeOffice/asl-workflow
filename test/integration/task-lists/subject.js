7a6467099f38eb72873c60d3cc1172c83c793870
Joe Fitter <joefitter@hotmail.com>
2019-12-16 16:04:54 +0000

    Added route to return open tasks for subject

---
+++ test/integration/task-lists/subject.js
@@ -0,0 +1,45 @@
+const request = require('supertest');
+
+const assertTasks = require('../../helpers/assert-tasks');
+const workflowHelper = require('../../helpers/workflow');
+
+const { user } = require('../../data/profiles');
+
+describe('Subject', () => {
+  before(() => {
+    return workflowHelper.create()
+      .then(workflow => {
+        this.workflow = workflow;
+        this.workflow.setUser({ profile: user });
+      });
+  });
+
+  beforeEach(() => {
+    return Promise.resolve()
+      .then(() => workflowHelper.resetDBs())
+      .then(() => workflowHelper.seedTaskList());
+  });
+
+  after(() => {
+    return workflowHelper.destroy();
+  });
+
+  it('returns open tasks for the subject', () => {
+    const expected = [
+      'recalled ppl',
+      'pil returned',
+      'pil with ntco',
+      'pil with licensing',
+      'profile update',
+      'another with-ntco to test ordering',
+      'project awaiting endorsement'
+    ];
+    return request(this.workflow)
+      .get(`/profile-tasks/${user.id}`)
+      .expect(200)
+      .expect(response => {
+        assertTasks(expected, response.body.data);
+      });
+  });
+
+});
