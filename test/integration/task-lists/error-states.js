33189e2a10839da68ecee5149cf2b3eda3418221
Leonard Martin <leonard.martin@gmail.com>
2020-01-28 12:43:47 +0000

    Whitelist progress states in task list builder
    If you request a task list with a broken progress parameter in the query string then it returns the default underlying query which returns _everything_ with no status filters (including autoresolved) which then breaks in the client.
    
    Instead return an empty list immediately if the progress parameter doesn't make an expected value.

---
+++ test/integration/task-lists/error-states.js
@@ -0,0 +1,45 @@
+const request = require('supertest');
+const assert = require('assert');
+
+const workflowHelper = require('../../helpers/workflow');
+
+const { user } = require('../../data/profiles');
+
+describe('Task list error states', () => {
+  before(() => {
+    return workflowHelper.create()
+      .then(workflow => {
+        this.workflow = workflow;
+      });
+  });
+
+  beforeEach(() => {
+    return Promise.resolve()
+      .then(() => workflowHelper.resetDBs())
+      .then(() => workflowHelper.seedTaskList());
+  });
+
+  after(() => {
+    return workflowHelper.destroy();
+  });
+
+  it('returns an empty list if no profile is found', () => {
+    this.workflow.setUser({ profile: null });
+    return request(this.workflow)
+      .get('/')
+      .expect(200)
+      .expect(response => {
+        assert.deepEqual(response.body.data, []);
+      });
+  });
+
+  it('returns an empty list if non-existent progress state is provided', () => {
+    this.workflow.setUser({ profile: user });
+    return request(this.workflow)
+      .get('/?progress=junk')
+      .expect(200)
+      .expect(response => {
+        assert.deepEqual(response.body.data, []);
+      });
+  });
+});
