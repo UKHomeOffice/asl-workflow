d2f419bb8ed5c24bcb6682ec048573b664ceb37d
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2020-06-26 16:05:14 +0100

    add activity log seed for discarded task

---
+++ test/data/activity.js
@@ -0,0 +1,25 @@
+const uuid = require('uuid/v4');
+const moment = require('moment');
+const ids = require('./ids');
+const { asruSuper } = require('./profiles');
+
+const generateDates = daysAgo => {
+  const date = moment().subtract(daysAgo, 'days').toISOString();
+  return {
+    createdAt: date,
+    updatedAt: date
+  };
+};
+
+const activity = [
+  {
+    id: uuid(),
+    caseId: ids.task.project.discardedByAsru,
+    changedBy: asruSuper.id,
+    eventName: 'status:with-inspectorate:discarded-by-asru',
+    event: {},
+    ...generateDates(34)
+  }
+];
+
+module.exports = activity;
