14cbe868e9337fbd6f6ba41ecb385a226a95a24d
Joe Fitter <joefitter@hotmail.com>
2020-11-09 13:59:35 +0100

    use moment business time to task bank hols into account

--- seeds/tables/cases.js
+++ seeds/tables/cases.js
@@ -1,14 +1,17 @@
 const uuidv4 = require('uuid/v4');
-const moment = require('moment');
+const moment = require('moment-business-time');
+const { bankHolidays } = require('@asl/constants');
 const cases = require('../data/cases.json');
+
+moment.updateLocale('en', { holidays: bankHolidays });
 
 module.exports = {
   populate: knex => {
     return Promise.all(cases.map(c => {
       if (c.id === '71bd42e1-7cd7-4d51-8d99-694bd4c14810') {
         // keep the dates current so that the deadline is in the future
-        c.created_at = moment().subtract(1, 'month').toISOString();
-        c.updated_at = moment().subtract(1, 'month').toISOString();
+        c.created_at = moment().subtractWorkingTime(1, 'month').toISOString();
+        c.updated_at = moment().subtractWorkingTime(1, 'month').toISOString();
       }
 
       return knex('cases')
